b93470ba2407ce95a38cc9be07cd0a4210f91ec6
Ryan Lee <ryantlee9@gmail.com>
2022-04-06 20:15:07 -0700

    Add time and granularity filters

--- app/__init__.py
+++ app/__init__.py
@@ -8,11 +8,11 @@ from flask_wtf import FlaskForm
 from wtforms import (
     StringField,
     SubmitField,
-    DecimalField,
 )
 from wtforms.validators import (
     Optional,
 )
+from wtforms_components import DateRange, DateField, DateTimeField
 from flask_session import Session
 from dotenv import load_dotenv
 import redis
@@ -37,6 +37,9 @@ table = dynamodb.Table("water_tank_sensor")
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z'
 app.config["FLASK_ENV"] = environ.get("FLASK_ENV", default="production")
+app.config['SESSION_TYPE'] = 'filesystem'
+# app.config["SESSION_TYPE"] = "redis"
+# app.config["SESSION_REDIS"] = r
 Session(app)
 
 data = []
@@ -50,7 +53,6 @@ while not done:
     app.logger.debug(f"Fetched {len(data_raw)} items from AWS DynamoDB")
     for item in data_raw:
         unix_time = int(item['sample_time'] / 1000)
-        print(unix_time)
         reading = SensorReading(
             datetime.fromtimestamp(unix_time, tz=time_zone),
             float(item['device_data']['sensor_depth_in']),
@@ -64,12 +66,61 @@ app.logger.info(f"Fetched {len(data)} items from AWS DynamoDB")
 df = pd.DataFrame(data).sort_values("time")
 df.time = pd.to_datetime(df.time)
 df.set_index('time', inplace=True)
-df_hourly = df.resample('H').mean()
-print(df_hourly)
+
+def toDate(dateString):
+    return datetime.strptime(dateString, "%Y-%m-%d").date()
+
+class SlabForm(FlaskForm):
+    start_time = DateField(validators=[Optional()])
+    end_time = DateField(validators=[DateRange(max=datetime.today().date()), Optional()])
+    granularity = StringField("Granularity")
+    submit = SubmitField("Submit")
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index", methods=["GET", "POST"])
 def index():
+
+    granularity = request.args.get("granularity", type=str)
+    end_time = request.args.get("end_time", type=toDate)
+    start_time = request.args.get("start_time", type=toDate)
+
+    filters = dict(
+        granularity=granularity,
+        end_time=end_time,
+        start_time=start_time,
+    )
+    # app.logger.info(f"Received filter request: {json.dumps(filters)}")
+    has_filters = any([value is not None for value in filters.values()])
+
+    # reset session if no args
+    if len(request.args) == 0:
+        app.logger.debug("Resetting session since no args found.")
+        if "filters" in session:
+            session.pop("filters")
+
+    session["filters"] = filters
+
+    form = SlabForm()
+
+    # filter form
+    if form.validate_on_submit():
+        redirect_kwargs = {
+            fieldname: form[fieldname].data
+            for fieldname in filters.keys()
+            if form[fieldname].data
+        }
+        return redirect(url_for("index", **redirect_kwargs))
+
+    # pre-populate form
+    for field in filters.keys():
+        form[field].data = filters[field]
+
+    # process data
+    df_filtered = df.copy()
+    if start_time:
+        df_filtered = df_filtered[pd.to_datetime(df_filtered.index.to_series()).dt.date >= start_time]
+    df_hourly = df_filtered.resample(granularity if granularity else 'H').mean()
+
     # parse args
     return render_template(
         "index.html",
@@ -77,4 +128,5 @@ def index():
         values=df_hourly.depth,
         labels=df_hourly.index,
         legend="Water depth",
+        form=form,
     )
